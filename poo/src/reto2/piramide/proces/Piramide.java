39cfd7e4c3314fe2b3015763e2f958871339323f
fabiansilvadelatorre <fabiansilvadelatorre@gmail.com>
2023-02-07 09:47:37 -0600

    Reto3

--- poo/src/reto2/piramide/proces/Piramide.java
+++ poo/src/reto2/piramide/proces/Piramide.java
@@ -1,5 +1,17 @@
 package reto2.piramide.proces;
-
+import reto2.piramide.ui.CLI;
+import reto2.piramide.Main;
 public class Piramide {
-
+public static void printPyramid(int numRenglon){
+    System.out.println();
+    for (int i =0;i<10;i++){
+        for (int j=0;j<i+1;j++){
+            if (i>5 && i<7){
+                continue;
+            }
+            System.out.println("*");
+        }
+        System.out.println();
+    }
+}
 }
